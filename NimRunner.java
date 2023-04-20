0f2faf094633807b352177b8216445060033874b
Kathryn Xia <kxia@greenwichacademy.org>
2023-04-20 13:52:51 -0400

    looking bad ya'll

--- NimRunner.java
+++ NimRunner.java
@@ -3,9 +3,38 @@ import java.util.*;
 
 public class NimRunner{
 
+  //will pass in a Node, not necesrry though
+
+  public int getX(Node root){
+    return -1;
+  }
+
+  public int getY(Node root){
+    return 1;
+  }
+
+  public static boolean minimax(int numStones, boolean myTurn){
+    if ((n == 0) && (myTurn == true)){
+      return true;
+    }
+
+    else if ((n == 1) && (myTurn == false)){
+      return false;
+    }
+
+    else{
+      return (miniMax());//snooped the internet, should return a multiple of 4, 2, 1
+    }
+
+
+  }
+
+
+
     /*
 
 run game returns boolean
+
 
 
 get X move --> int
@@ -13,6 +42,12 @@ get y move --> int
 display method (state of game)
 
 pretend x is player 1
+
+1. hard code valid moves for now (only three)
+2. hard code winning points
+3. use algorithm, and do subtraction to to get winning points
+ - find points where the cumputer can get into "winning position" aka put othewr player into losiugn positiojn
+  - hard code these states into the computer
 
 
 */
